a00d7b3eeb0d3a76f90b1b351e77e7dc45ff1105
UvinduV <uvindusanketh470@gmail.com>
2024-08-31 10:38:25 +0530

    create config class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import org.example.config.config;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-
+        var ctx =new AnnotationConfigApplicationContext();
+        ctx.register(config.class);
     }
 }
